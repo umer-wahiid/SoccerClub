0f649bd87b788c7952342111c2196db646c0b610
Umer Abdul Wahid & Ammar Ansari <umerwahiid48@gmail.com>
2023-09-17 18:01:19 -0700

    Match Details Done

--- SoccerClub/SoccerClub/Controllers/HomeController.cs
+++ SoccerClub/SoccerClub/Controllers/HomeController.cs
@@ -12,262 +12,274 @@ using System.Net.Http.Json;
 
 namespace SoccerClub.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly SoccerClubContext db;
-		private readonly IApiHelper _apiHelper;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SoccerClubContext db;
+        private readonly IApiHelper _apiHelper;
 
-		public HomeController(ILogger<HomeController> logger, SoccerClubContext db, IApiHelper apiHelper)
-		{
-			_logger = logger;
-			this.db = db;
-			this._apiHelper = apiHelper;
-		}
-       
+        public HomeController(ILogger<HomeController> logger, SoccerClubContext db, IApiHelper apiHelper)
+        {
+            _logger = logger;
+            this.db = db;
+            this._apiHelper = apiHelper;
+        }
+
         public IActionResult TopTen()
-		
-		{
 
-			var wcResponse = _apiHelper.TopTenScores("WC");
-			wcResponse = wcResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
-			Root wcApiResponse = JsonConvert.DeserializeObject<Root>(wcResponse);
+        {
 
-			var clResponse = _apiHelper.TopTenScores("SA");
-			clResponse = clResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
-			Root clApiResponse = JsonConvert.DeserializeObject<Root>(clResponse);
+            var wcResponse = _apiHelper.TopTenScores("WC");
+            wcResponse = wcResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
+            Root wcApiResponse = JsonConvert.DeserializeObject<Root>(wcResponse);
 
-			var plResponse = _apiHelper.TopTenScores("PL");
-			plResponse = plResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
-			Root plApiResponse = JsonConvert.DeserializeObject<Root>(plResponse);
+            var clResponse = _apiHelper.TopTenScores("SA");
+            clResponse = clResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
+            Root clApiResponse = JsonConvert.DeserializeObject<Root>(clResponse);
 
-			var topTenScorersViewModel = new IndexVM
-			{
-                WorldCup= wcApiResponse,
+            var plResponse = _apiHelper.TopTenScores("PL");
+            plResponse = plResponse.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
+            Root plApiResponse = JsonConvert.DeserializeObject<Root>(plResponse);
+
+            var topTenScorersViewModel = new IndexVM
+            {
+                WorldCup = wcApiResponse,
                 Premier = plApiResponse,
                 UEFA = clApiResponse
             };
             var allTopScorers = topTenScorersViewModel.GetType().GetProperties()
             .Where(prop => prop.PropertyType == typeof(Root))
             .ToList();
-           ViewBag.TopTenScores = allTopScorers;
-                return View(topTenScorersViewModel);
-		}
+            ViewBag.TopTenScores = allTopScorers;
+            return View(topTenScorersViewModel);
+        }
 
-		public IActionResult Index()
-		{
+        public IActionResult Index()
+        {
             ViewBag.Home = "toactive";
-			//var jsonContent = _apiHelper.GetRecentUpdates();
-			//jsonContent = jsonContent.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
-			//var jsonObject = JObject.Parse(jsonContent);
-			//var articlesArray = jsonObject["articles"].ToString();
+            //var jsonContent = _apiHelper.GetRecentUpdates();
+            //jsonContent = jsonContent.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
+            //var jsonObject = JObject.Parse(jsonContent);
+            //var articlesArray = jsonObject["articles"].ToString();
 
-			//List<Article> responseList = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
+            //List<Article> responseList = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
+            var UpComming = db.Matches.Include(x => x.AwayTeam).Include(x => x.HomeTeam).Where(m => m.Date > DateTime.Now).OrderBy(m => m.Date).FirstOrDefault();
 
-			var ViewModel = new IndexVM
-			{
-				match = db.Matches.OrderByDescending(m => m.MatchId).Take(4).ToList(),
-				matches = db.Matches.OrderByDescending(m => m.MatchId).Take(5).ToList(),
-				player = db.Players.ToList(),
-				team = db.Teams.ToList(),
-				product = db.Products.ToList()
-				//Articles = responseList
-
-			};
-			return View(ViewModel);
-		}
-		public ActionResult Search(string keyword)
-		{
-			ViewBag.keyword = keyword;
-			var ViewModel = new IndexVM
-			{
-				matches = db.Matches.Include(x => x.AwayTeam).Include(x => x.HomeTeam).Where(m => m.AwayTeam.Name.Contains(keyword) || m.HomeTeam.Name.Contains(keyword)).ToList(),
-				player = db.Players.Include(x => x.Team).Where(m => m.Name.Contains(keyword)).ToList(),
-				team = db.Teams.Where(m => m.Name.Contains(keyword)).ToList(),
-				product = db.Products.Where(m => m.ProductName.Contains(keyword)).ToList(),
-			};
-			return View(ViewModel);
-		}
-		public async Task<IActionResult> ProductDetail(int? id)
-		{
+            if(UpComming != null)
+            {
+                ViewBag.HomeTeam = UpComming.HomeTeam.Name;
+                ViewBag.AwayTeam = UpComming.AwayTeam.Name;
+                ViewBag.date = UpComming.Date.ToLongTimeString();
+            }
+            var ViewModel = new IndexVM
+            {
+                match = db.Matches.OrderByDescending(m => m.MatchId).Take(4).ToList(),
+                matches = db.Matches.OrderByDescending(m => m.MatchId).Take(5).ToList(),
+                player = db.Players.ToList(),
+                team = db.Teams.ToList(),
+                product = db.Products.ToList()
+                //Articles = responseList
+            };
+            return View(ViewModel);
+        }
+        public ActionResult Search(string keyword)
+        {
+            ViewBag.keyword = keyword;
+            var ViewModel = new IndexVM
+            {
+                matches = db.Matches.Include(x => x.AwayTeam).Include(x => x.HomeTeam).Where(m => m.AwayTeam.Name.Contains(keyword) || m.HomeTeam.Name.Contains(keyword)).ToList(),
+                player = db.Players.Include(x => x.Team).Where(m => m.Name.Contains(keyword)).ToList(),
+                team = db.Teams.Where(m => m.Name.Contains(keyword)).ToList(),
+                product = db.Products.Where(m => m.ProductName.Contains(keyword)).ToList(),
+            };
+            return View(ViewModel);
+        }
+        public async Task<IActionResult> ProductDetail(int? id)
+        {
             ViewBag.shop = "toactive";
             if (id == null || db.Products == null)
-			{
-				return NotFound();
-			}
+            {
+                return NotFound();
+            }
 
-			var product = await db.Products
-				.Include(p => p.Category)
-				.FirstOrDefaultAsync(m => m.ProductId == id);
-			if (product == null)
-			{
-				return NotFound();
-			}
+            var product = await db.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
 
-			return View(product);
-		}
+            return View(product);
+        }
 
-		public IActionResult Matches()
-		{
-			ViewBag.matches = "toactive";
-			var match = db.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam).
-				ToList();
-			return View(match);
-		}
-		// GET: Matches/Details/5
-		public async Task<IActionResult> MatchesDetail(int? id)
-		{
+        public IActionResult Matches()
+        {
+            ViewBag.matches = "toactive";
+            var match = db.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam).
+                ToList();
+            return View(match);
+        }
+        // GET: Matches/Details/5
+        public async Task<IActionResult> MatchesDetail(int? id)
+        {
             ViewBag.matches = "toactive";
             if (id == null || db.Matches == null)
-			{
-				return NotFound();
-			}
+            {
+                return NotFound();
+            }
 
-			var match = await db.Matches
-				.Include(m => m.AwayTeam)
-				.Include(m => m.HomeTeam)
-				.FirstOrDefaultAsync(m => m.MatchId == id);
-			if (match == null)
-			{
-				return NotFound();
-			}
+            var match = await db.Matches
+                .Include(m => m.AwayTeam)
+                .Include(m => m.HomeTeam)
+                .FirstOrDefaultAsync(m => m.MatchId == id);
+            ViewBag.MatchDetails = match;
+            var HomePlayers = await db.Players.Where(m => m.TeamId == match.HomeTeamId).ToListAsync();
+            var AwayPlayers = await db.Players.Where(m => m.TeamId == match.AwayTeamId).ToListAsync();
 
-			return View(match);
-		}
-		public async Task<IActionResult> Shop()
-		{
+            var ViewModel = new IndexVM
+            {
+                player = (IEnumerable<Player>)HomePlayers.ToList(),
+                awayplayer = (IEnumerable<Player>)AwayPlayers.ToList(),
+                Match = match
+            };
+
+            return View(ViewModel);
+        }
+        public async Task<IActionResult> Shop()
+        {
             ViewBag.shop = "toactive";
             ViewBag.CategoryId = db.Category.ToList();
-			return View(await db.Products.ToListAsync());
-		}
+            return View(await db.Products.ToListAsync());
+        }
 
-		public IActionResult Schedule()
-		{
+        public IActionResult Schedule()
+        {
             ViewBag.schedule = "toactive";
-			var match = db.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam).
-				ToList();
-			return View(match);
-		}
-		public IActionResult Blog()
-		{
+            var match = db.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam).
+                ToList();
+            return View(match);
+        }
+        public IActionResult Blog()
+        {
             ViewBag.blog = "toactive";
             var jsonContent = _apiHelper.GetRecentUpdates();
-			jsonContent = jsonContent.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
-			var jsonObject = JObject.Parse(jsonContent);
-			var articlesArray = jsonObject["articles"].ToString();
+            jsonContent = jsonContent.Replace("\r\n", "").Replace("\n", "").Replace("\t", "").Replace("\r", "");
+            var jsonObject = JObject.Parse(jsonContent);
+            var articlesArray = jsonObject["articles"].ToString();
 
-			List<Article> responseList = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
+            List<Article> responseList = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
 
-			return View(responseList);
-		}
-		public IActionResult BlogDetails(string url)
-		{
+            return View(responseList);
+        }
+        public IActionResult BlogDetails(string url)
+        {
             ViewBag.blog = "toactive";
             var jsonContent = _apiHelper.GetRecentUpdates();
-			var jsonObject = JObject.Parse(jsonContent);
-			var articlesArray = jsonObject["articles"].ToString();
-			List<Article> articles = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
+            var jsonObject = JObject.Parse(jsonContent);
+            var articlesArray = jsonObject["articles"].ToString();
+            List<Article> articles = JsonConvert.DeserializeObject<List<Article>>(articlesArray);
 
-			Article article = articles.FirstOrDefault(a => a.Url == url);
-			if (article != null)
-			{
-				return View(article);
-			}
-			return View();
+            Article article = articles.FirstOrDefault(a => a.Url == url);
+            if (article != null)
+            {
+                return View(article);
+            }
+            return View();
 
-			// Handle article not found case (e.g., redirect to an error page)
-		}
-		public IActionResult Players()
-		{
+            // Handle article not found case (e.g., redirect to an error page)
+        }
+        public IActionResult Players()
+        {
             ViewBag.pl = "toactive";
-            return View(db.Players.Include(x=>x.Team).ToList());
-		}
+            return View(db.Players.Include(x => x.Team).ToList());
+        }
 
-		public async Task<IActionResult> PlayersDetail(int? id)
-		{
-            
+        public async Task<IActionResult> PlayersDetail(int? id)
+        {
+
             if (id == null || db.Players == null)
-			{
-				return NotFound();
-			}
+            {
+                return NotFound();
+            }
             ViewBag.players = "toactive";
             var player = await db.Players.Include(p => p.Team).FirstOrDefaultAsync(m => m.PlayerID == id);
-			if (player == null)
-			{
-				return NotFound();
-			}
+            if (player == null)
+            {
+                return NotFound();
+            }
 
-			return View(player);
-		}
+            return View(player);
+        }
 
-		[HttpGet]
-		public IActionResult Contact()
-		{
+        [HttpGet]
+        public IActionResult Contact()
+        {
             ViewBag.contact = "toactive";
             return View();
-		}
+        }
 
-		[HttpPost]
-		[ActionName("Contact")]
-		public IActionResult Contact(ContactUs contactUs)
-		{
-			if (ModelState.IsValid)
-			{
-				db.contactUs.Add(contactUs);
-				db.SaveChanges();
-				ViewBag.ContactMsg = "Thanks For Reaching Out";
+        [HttpPost]
+        [ActionName("Contact")]
+        public IActionResult Contact(ContactUs contactUs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contactUs.Add(contactUs);
+                db.SaveChanges();
+                ViewBag.ContactMsg = "Thanks For Reaching Out";
 
-				return View();
-			}
-			return View(contactUs);
-		}
+                return View();
+            }
+            return View(contactUs);
+        }
 
-		[HttpGet]
-		public IActionResult Feedback()
-		{
+        [HttpGet]
+        public IActionResult Feedback()
+        {
             ViewBag.feedbackActive = "toactive";
             if (Session.UserId != 0)
-			{
+            {
 
-				ViewBag.feedback = "";
+                ViewBag.feedback = "";
                 return View();
-			}
-			return RedirectToAction("Login", "Users");
-		}
+            }
+            return RedirectToAction("Login", "Users");
+        }
 
-		[HttpPost]
-		public IActionResult Feedback(Feedback feed)
-		{
-			feed.UserId = Session.UserId;
-			db.Feedbacks.Add(feed);
-			db.SaveChanges();
-			ViewBag.feedback = "Thanks For Your Feedback";
+        [HttpPost]
+        public IActionResult Feedback(Feedback feed)
+        {
+            feed.UserId = Session.UserId;
+            db.Feedbacks.Add(feed);
+            db.SaveChanges();
+            ViewBag.feedback = "Thanks For Your Feedback";
 
-			return View();
-		}
+            return View();
+        }
 
-		public IActionResult Cart()
-		{
+        public IActionResult Cart()
+        {
             ViewBag.cart = "toactive";
             return View();
-		}
+        }
 
-		public IActionResult Login()
-		{
-			return View();
-		}
-		public IActionResult History()
-		{
-			ViewBag.history = "toactive";
-			return View();
-		}
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult History()
+        {
+            ViewBag.history = "toactive";
+            return View();
+        }
 
 
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
--- SoccerClub/SoccerClub/Models/IndexVM.cs
+++ SoccerClub/SoccerClub/Models/IndexVM.cs
@@ -11,7 +11,9 @@ namespace SoccerClub.Models
         public IEnumerable<Team> team { get; set; }
         public IEnumerable<Product> product { get; set; }
         public IEnumerable<Player> player { get; set; }
+        public IEnumerable<Player> awayplayer { get; set; }
         public IEnumerable<Article> Articles { get; set; }
+        public Match Match { get; set; }
         public Root WorldCup { get; set; }
         public Root UEFA { get; set; }
         public Root Premier { get; set; }
